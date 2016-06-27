fc677781109b0f8ee552290c55688ea61496a47e
Michael Caughron <michael.caughron@energychoice.com>
2016-06-27 09:41:07 -0400

    commit for saving

--- src/components/Card.js
+++ src/components/Card.js
@@ -43,20 +43,37 @@ class Card extends Component {
   log_call() {
       this.setState({ showModal: false });
       var card = this
-      var lead_prompt_info = [
-        {label: "Contact", fieldtype: "Data", default:this.props.doc.lead_name, read_only: "1"},
-        {fieldtype: "Column Break"},
-        {label: "Phone", fieldname: "phone_no", fieldtype: "Data", default: this.props.doc.phone, read_only: "1"},
-        {fieldtype: "Section Break"},
-        {label: "Sent or Received", fieldname: "sent_or_received",
-        fieldtype: "Select", options: ["Sent", "Received"], default: "Sent"},
-        {fieldtype: "Column Break"},
-        {label: "Next Contact Date", fieldtype: "Date", reqd: "1"},
-        {fieldtype: "Section Break"},
-        {label: "Subject", fieldtype: "Data", default: "Call " + (new Date())},
-        {fieldtype: "Section Break"},
-        {label: "Notes", fieldname: "content", fieldtype: "Small Text", reqd:"1"}
-      ]
+      var prompt_info = []
+      if (this.props.doc.contact_date === undefined) {
+        prompt_info = [
+          {label: "Contact", fieldtype: "Data", default:this.props.doc.lead_name, read_only: "1"},
+          {fieldtype: "Column Break"},
+          {label: "Phone", fieldname: "phone_no", fieldtype: "Data", default: this.props.doc.phone, read_only: "1"},
+          {fieldtype: "Section Break"},
+          {label: "Sent or Received", fieldname: "sent_or_received",
+          fieldtype: "Select", options: ["Sent", "Received"], default: "Sent"},
+          {fieldtype: "Section Break"},
+          {label: "Subject", fieldtype: "Data", default: "Call " + (new Date())},
+          {fieldtype: "Section Break"},
+          {label: "Notes", fieldname: "content", fieldtype: "Small Text", reqd:"1"}
+        ]
+      }
+      else {
+        prompt_info = [
+          {label: "Contact", fieldtype: "Data", default:this.props.doc.lead_name, read_only: "1"},
+          {fieldtype: "Column Break"},
+          {label: "Phone", fieldname: "phone_no", fieldtype: "Data", default: this.props.doc.phone, read_only: "1"},
+          {fieldtype: "Section Break"},
+          {label: "Sent or Received", fieldname: "sent_or_received",
+          fieldtype: "Select", options: ["Sent", "Received"], default: "Sent"},
+          {fieldtype: "Column Break"},
+          {label: "Next Contact Date", fieldtype: "Date", reqd: "1"},
+          {fieldtype: "Section Break"},
+          {label: "Subject", fieldtype: "Data", default: "Call " + (new Date())},
+          {fieldtype: "Section Break"},
+          {label: "Notes", fieldname: "content", fieldtype: "Small Text", reqd:"1"}
+        ]
+      }
       frappe.call({
         method: "frappe.client.get_list",
         args: {
@@ -69,7 +86,7 @@ class Card extends Component {
           }
         },
         callback: function(r) {
-          lead_prompt_info.push({fieldtype: "Section Break"})
+          prompt_info.push({fieldtype: "Section Break"})
           var newMessage = "new"
           if(r.message != undefined) {
             r.message.forEach(function(foo) {
@@ -90,18 +107,18 @@ class Card extends Component {
                               fieldtype: "Small Text", default: content, read_only: "1"}
               }
               var state = true
-              lead_prompt_info.forEach(function(bar) {
+              prompt_info.forEach(function(bar) {
                 if(JSON.stringify(bar) == JSON.stringify(newMessage)) {
                   state = false
                 }
               })
               if(state) {
-                lead_prompt_info.push(newMessage)
-                lead_prompt_info.push({fieldtype: "Section Break"})
+                prompt_info.push(newMessage)
+                prompt_info.push({fieldtype: "Section Break"})
               }
             })
           }
-          var communication = frappe.prompt(lead_prompt_info,
+          var communication = frappe.prompt(prompt_info,
             function(data){
               data.doctype = "Communication";
               data.reference_doctype = card.props.doc.doctype;
@@ -112,15 +129,18 @@ class Card extends Component {
                   "doc": data
                 },
                 callback: function(r) {
-                  frappe.call({
-                    method: "frappe.client.set_value",
-                    args: {
-                      "doctype": card.props.doc.doctype,
-                      "name": card.props.doc.name,
-                      "fieldname": "contact_date",
-                      "value": data.next_contact_date
-                    }
-                  })
+                  if (card.props.doc.contact_date !== undefined) {
+                    frappe.call({
+                      method: "frappe.client.set_value",
+                      args: {
+                        "doctype": card.props.doc.doctype,
+                        "name": card.props.doc.name,
+                        "fieldname": "contact_date",
+                        "value": data.next_contact_date
+                      }
+                    })
+                  }
+
                 }
               })
             })
